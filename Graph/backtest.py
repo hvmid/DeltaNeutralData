2640268a2ba4742548f1bd46b011a7db0bfb39a5
Hamid Yaghchi <hamid.zade@icloud.com>
2022-04-18 15:18:44 -0400

    Readme added

--- Graph/backtest.py
+++ Graph/backtest.py
@@ -1,52 +1,62 @@
 import pandas as pd
-from datetime import datetime
 import os
 
-def topPairs(top):
 
-	os.system("node mostLiquid.js %i" %top)
-	datafile = "topPairs.csv"
-	df = pd.read_csv(datafile)
+def topPairs(n):
+    # n: number of top pairs
+    # Runs node script: "mostLiquid.js"
+    # Returns the DataFrame with top n pairs
+    # Columns: (token0, token1, reserveUSD, pairAddress)
+    # Data is saved into a csv table "topPairs.csv"
 
-	return df
+    os.system("node mostLiquid.js %i" % n)
+    datafile = "topPairs.csv"
+    df = pd.read_csv(datafile)
+
+    return df
+
 
 def backtest(pairAddress, days, rate):
 
-	os.system("node graph.js %s %i" %(pairAddress, days))
-	datafile = "data/%s.csv" %pairAddress
-	df = pd.read_csv(datafile)
+    os.system("node graph.js %s %i" % (pairAddress, days))
+    datafile = "data/%s.csv" % pairAddress
+    df = pd.read_csv(datafile)
 
-	pairApr = apr(rate, df)
+    pairApr = apr(rate, df)
 
-	return pairApr
+    return pairApr
+
 
 def apr(rate, df):
-	df['24hr_reward'] = df['dailyVolumeUSD']*rate
-	df['pool_share'] = 1/df['reserveUSD']
-	df['24hr_reward_share'] = df['24hr_reward']*df['pool_share']
-	backtest_apr = sum(df['24hr_reward_share'])
-	backtest_apy = apy(backtest_apr)
+    df['24hr_reward'] = df['dailyVolumeUSD'] * rate
+    df['pool_share'] = 1 / df['reserveUSD']
+    df['24hr_reward_share'] = df['24hr_reward'] * df['pool_share']
+    backtest_apr = sum(df['24hr_reward_share'])
+    backtest_apy = apy(backtest_apr)
 
-	return backtest_apr, backtest_apy
+    return backtest_apr, backtest_apy
+
 
 def apy(apr):
 
-	return ((1 + apr/365)**365)-1
+    return ((1 + apr / 365)**365) - 1
 
-def do_backtest(top, days, rate):
 
-	aprs  = []
-	apys = []
-	df = topPairs(top)
-	pairs = df['pairAddress']
+def topPairs_backtest(top, days, rate):
 
-	for pair in pairs:
-		aprs.append(backtest(pair, days, rate)[0])
-		apys.append(backtest(pair, days, rate)[1])
-	
-	df["aprs"]=aprs
-	df["apys"]=apys
-	df.to_csv("backtest%sdays.csv" %days)
+    aprs = []
+    apys = []
+    df = topPairs(top)
+    pairs = df['pairAddress']
 
-do_backtest(100, 7, 0.003)
+    for pair in pairs:
+        aprs.append(backtest(pair, days, rate)[0])
+        apys.append(backtest(pair, days, rate)[1])
 
+    df["aprs"] = aprs
+    df["apys"] = apys
+    df.to_csv("backtest%sdays.csv" % days)
+
+
+if __name__ == "main":
+    topPairs_backtest(100, 7, 0.003)
--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # DeltaNeutralData
-On Chain Analytics
+Uniswap V2: Backtesting Liquidity pools
+
+Dependencies: 
+- node v17.9
+- npm v8.5.5
+- Python v3.10
+- pandas
+
+How to use:
+
+Run the Python script and topPair_backtest will be called, you can change the input arguments: topPair_backtest(n, days, rate)
+n - number of top pair (ordered by reserveUSD (liquidity) descending)
+days - number of days to backtest
+rate - current exchange fee for the given liquidity pool
+
+Output:
+
+1. Data (folder): contains csv tables with pairDayData (reserveUSD, dailyVolumeUSD, etc.) for a given pair, csv tables names are 
+each pairs' pairAddress.
+
+2. topPairs.csv: contains the outout of topPairs.js. top n pairs ordered by reserveUSD descending.
+
+3. backtest[n]days.csv: contains the final product of the backtest running n days.
